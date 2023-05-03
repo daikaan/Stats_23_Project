ca0775f381cb23f59bdfb59dd190d9826750d51f
canalgan35 <55834751+canalgan35@users.noreply.github.com>
2023-05-03 16:56:04 +0200

    Update stat_r.R

--- stat_r.R
+++ stat_r.R
@@ -1,14 +1,22 @@
-bank_data <- read.csv('~/GitHub/Stats_23_Project/BankChurners.csv')
+bank_data <- read.csv('C:/Users/Bertug/Desktop/BankChurners.csv')
 
 head(bank_data)
 dim(bank_data) #10127 rows and 23 columns
 
-col = list('CLIENTNUM', 'Attrition_Flag', 'Customer_Age',	'Gender',	'Dependent_count', 'Education_Level',	'Marital_Status',	'Income_Category', 'Card_Category',	'Months_on_book',	'Total_Relationship_Count', 'Months_Inactive_12_mon',	'Contacts_Count_12_mon', 'Credit_Limit', 'Total_Revolving_Bal',	'Avg_Open_To_Buy', 'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt', 'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1',	'Avg_Utilization_Ratio',	'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1',	'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2')
+col = c('CLIENTNUM', 'Attrition_Flag', 'Customer_Age',	'Gender',	'Dependent_count', 'Education_Level',	'Marital_Status',	'Income_Category', 'Card_Category',	'Months_on_book',	'Total_Relationship_Count', 'Months_Inactive_12_mon',	'Contacts_Count_12_mon', 'Credit_Limit', 'Total_Revolving_Bal',	'Avg_Open_To_Buy', 'Total_Amt_Chng_Q4_Q1', 'Total_Trans_Amt', 'Total_Trans_Ct', 'Total_Ct_Chng_Q4_Q1',	'Avg_Utilization_Ratio',	'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1',	'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2')
 
+library(knitr)
+library(forecast)
+library(ggplot2)
 library(PCAmixdata)
+
 data.split <- splitmix(bank_data)
 quantitative <- data.split$X.quanti
 qualitative <- data.split$X.quali
 
 length(quantitative)
 length(qualitative)
+
+colSums(is.na(bank_data)) #no null value
+
+hist(bank_data$Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1, main = "Churn", xlab = "Churn Seg", ylab = "Count", col = "cornflowerblue")
