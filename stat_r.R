ae1384e153afa70020e730c953137d2745de6720
Kaan Daibasoglu <83865805+daikaan@users.noreply.github.com>
2023-06-05 12:18:15 +0300

    Update stat_r.R
    all descriptive graphs to main

--- stat_r.R
+++ stat_r.R
@@ -15,16 +15,34 @@ library(PCAmixdata)
 library(dplyr)
 
 
-
-data.split <- splitmix(bank_data)
-quantitative <- data.split$X.quanti
-qualitative <- data.split$X.quali
-
-length(quantitative)
-length(qualitative)
-
 #Checking if there is any null value in the data set
 colSums(is.na(bank_data)) #no null value
+
+#Customer age
+
+#boxplot
+cust.age.boxplot <- boxplot(quantitative$Customer_Age, ylab = "age")
+cust.age.boxplot
+
+#using the 1st quartile-1.5*IQR and 3rd quartile+1.5*IQR rule, 
+#it is seen that customers over the age of 70 are outliers
+age.exc.list <- boxplot.stats(quantitative$Customer_Age)$out
+
+quantitative_less70 <- subset(quantitative,!(Customer_Age %in% age.exc.list))
+boxplot(quantitative_less70$Customer_Age, ylab = "age")
+
+#Card Category
+ggplot(bank_data, aes(x=Months_on_book, y= Credit_Limit, shape = Card_Category, color= Card_Category))+
+  geom_point() + geom_smooth(method=lm, se=FALSE, fullrange=TRUE)
+
+card.exc.list <- c("Silver", "Platinum", "Gold")
+
+#bank_data_bluecard <- subset(bank_data,!(Card_Category %in% card.exc.list))
+
+
+bank_data_cleaned <- subset(bank_data,!((Customer_Age %in% age.exc.list)| (Card_Category %in% card.exc.list)))
+
+boxplot(bank_data_cleaned$Customer_Age, ylab = "age")
 
 # Descriptive Graphs
 #histogram
@@ -121,9 +139,15 @@ hist(bank_data_cleaned$Total_Ct_Chng_Q4_Q1)
 #histogram
 hist(bank_data_cleaned$Avg_Utilization_Ratio)
 
-unique(bank_data$Attrition_Flag) #to make sure there are only 2 strings
+unique(bank_data_cleaned$Attrition_Flag) #to make sure there are only 2 strings
 #change 'Existing Customer' to 1 and 'Attrited Customer' to 0 and add new column to quantitative
-quantitative$attrition_flag_binary <- ifelse(bank_data$Attrition_Flag=='Existing Customer', 1, 0)
+data.split <- splitmix(bank_data_cleaned)
+quantitative <- data.split$X.quanti
+qualitative <- data.split$X.quali
+
+length(quantitative)
+length(qualitative)
+quantitative$attrition_flag_binary <- ifelse(bank_data_cleaned$Attrition_Flag=='Existing Customer', 1, 0)
 
 #grouped age histogram
 Grouped.age.hist <- hist(as.numeric(quantitative$age_group), xlab="age_group", ylab="freq", breaks=4,
@@ -153,137 +177,3 @@ hist(bank_data$Contacts_Count_12_mon, col = yarrr::transparent('blue', trans.val
 
 hist(bank_data$Total_Trans_Ct)
 
-#investigate correlation
-#we can add one column after we decide conditions as churn number
-#and we can investigate the correlation this column with other columns
-library(corrplot)
-corr_quant <- subset(quantitative, select = -c(CLIENTNUM, Dependent_count, age_group, Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1, Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2))
-corralted = cor(corr_quant)
-corrplot(corralted, method = 'color', order = 'alphabet')
-
-#create copy of qualitative data and make it quantitative
-qual_to_quant <- qualitative
-
-#change categories to the numeric values
-qual_to_quant$Attrition_Flag <- ifelse(qual_to_quant$Attrition_Flag == 'Existing Customer', 1, 0)
-
-qual_to_quant$Gender <- ifelse(qual_to_quant$Gender == 'M', 1, 0)
-
-unique(bank_data$Education_Level)
-qual_to_quant$Education_Level[qual_to_quant$Education_Level == 'Unknown'] <- 0
-qual_to_quant$Education_Level[qual_to_quant$Education_Level == 'Uneducated'] <- 1
-qual_to_quant$Education_Level[qual_to_quant$Education_Level == 'High School'] <- 2
-qual_to_quant$Education_Level[qual_to_quant$Education_Level == 'College'] <- 3
-qual_to_quant$Education_Level[qual_to_quant$Education_Level == 'Graduate'] <- 4
-qual_to_quant$Education_Level[qual_to_quant$Education_Level == 'Post-Graduate'] <- 5
-qual_to_quant$Education_Level[qual_to_quant$Education_Level == 'Doctorate'] <- 6
-qual_to_quant$Education_Level = as.numeric(as.character(qual_to_quant$Education_Level))
-
-unique(bank_data$Marital_Status)
-qual_to_quant$Marital_Status[qual_to_quant$Marital_Status == 'Unknown'] <- 0
-qual_to_quant$Marital_Status[qual_to_quant$Marital_Status == 'Single'] <- 1
-qual_to_quant$Marital_Status[qual_to_quant$Marital_Status == 'Married'] <- 2
-qual_to_quant$Marital_Status[qual_to_quant$Marital_Status == 'Divorced'] <- 3
-qual_to_quant$Marital_Status = as.numeric(as.character(qual_to_quant$Marital_Status))
-
-unique(bank_data$Income_Category)
-qual_to_quant$Income_Category[qual_to_quant$Income_Category == 'Unknown'] <- 0
-qual_to_quant$Income_Category[qual_to_quant$Income_Category == 'Less than $40K'] <- 1
-qual_to_quant$Income_Category[qual_to_quant$Income_Category == '$40K - $60K'] <- 2
-qual_to_quant$Income_Category[qual_to_quant$Income_Category == '$60K - $80K'] <- 3
-qual_to_quant$Income_Category[qual_to_quant$Income_Category == '$80K - $120K'] <- 4
-qual_to_quant$Income_Category[qual_to_quant$Income_Category == '$120K +'] <- 5
-qual_to_quant$Income_Category = as.numeric(as.character(qual_to_quant$Income_Category))
-
-unique(bank_data$Card_Category)
-qual_to_quant$Card_Category[qual_to_quant$Card_Category == 'Silver'] <- 0
-qual_to_quant$Card_Category[qual_to_quant$Card_Category == 'Gold'] <- 1
-qual_to_quant$Card_Category[qual_to_quant$Card_Category == 'Platinum'] <- 2
-qual_to_quant$Card_Category[qual_to_quant$Card_Category == 'Blue'] <- 3
-qual_to_quant$Card_Category = as.numeric(as.character(qual_to_quant$Card_Category))
-
-corr_qual_to_quant = cor(qual_to_quant)
-corrplot(corr_qual_to_quant, method = 'number')
-
-#function to normalize value
-normalizer_fnc <- function(x) {
-  (x - min(x)) / (max(x) - min(x))
-}
-
-quan_normalized <- as.data.frame(lapply(quantitative[2:18], normalizer_fnc))
-qual_to_quant_normalized <- as.data.frame(lapply(qual_to_quant[2:6], normalizer_fnc))
-
-
-
-#we need to calculate chi square for the categorical values 
-#to see are they dependent or not
-#assume conf interval 95%
-table(quantitative$attrition_flag_binary, quantitative$age_group)
-chisq.test(quantitative$attrition_flag_binary, quantitative$age_group, correct=FALSE)
-
-table(quantitative$attrition_flag_binary, quantitative$Dependent_count)
-chisq.test(quantitative$attrition_flag_binary, quantitative$Dependent_count, correct=FALSE)
-
-table(quantitative$attrition_flag_binary, quantitative$Total_Relationship_Count)
-chisq.test(quantitative$attrition_flag_binary, quantitative$Total_Relationship_Count, correct=FALSE)
-
-table(quantitative$attrition_flag_binary, quantitative$Contacts_Count_12_mon)
-chisq.test(quantitative$attrition_flag_binary, quantitative$Contacts_Count_12_mon, correct=FALSE)
-
-table(qual_to_quant$Attrition_Flag, qual_to_quant$Gender)
-chisq.test(qual_to_quant$Attrition_Flag, qual_to_quant$Gender, correct=FALSE)
-
-table(qual_to_quant$Attrition_Flag, qual_to_quant$Education_Level)
-chisq.test(qual_to_quant$Attrition_Flag, qual_to_quant$Education_Level, correct=FALSE)
-
-table(qual_to_quant$Attrition_Flag, qual_to_quant$Marital_Status)
-chisq.test(qual_to_quant$Attrition_Flag, qual_to_quant$Marital_Status, correct=FALSE)
-
-table(qual_to_quant$Attrition_Flag, qual_to_quant$Income_Category)
-chisq.test(qual_to_quant$Attrition_Flag, qual_to_quant$Income_Category, correct=FALSE)
-
-#simulate.p.value
-table(qual_to_quant$Attrition_Flag, qual_to_quant$Card_Category)
-chisq.test(qual_to_quant$Attrition_Flag, qual_to_quant$Card_Category, correct=FALSE, simulate.p.value=TRUE)
-
-table(quantitative$attrition_flag_binary, quantitative$Months_Inactive_12_mon)
-chisq.test(quantitative$attrition_flag_binary, quantitative$Months_Inactive_12_mon, correct=FALSE, simulate.p.value=TRUE)
-
-table(quantitative$attrition_flag_binary, quantitative$Months_on_book)
-chisq.test(quantitative$attrition_flag_binary, quantitative$Months_on_book, correct=FALSE, simulate.p.value=TRUE)
-
-table(quantitative$attrition_flag_binary, quantitative$Credit_Limit)
-chisq.test(quantitative$attrition_flag_binary, quantitative$Credit_Limit, correct=FALSE, simulate.p.value=TRUE)
-
-table(quantitative$attrition_flag_binary, quantitative$Total_Revolving_Bal)
-chisq.test(quantitative$attrition_flag_binary, quantitative$Total_Revolving_Bal, correct=FALSE, simulate.p.value=TRUE)
-
-table(quantitative$attrition_flag_binary, quantitative$Avg_Open_To_Buy)
-chisq.test(quantitative$attrition_flag_binary, quantitative$Avg_Open_To_Buy, correct=FALSE, simulate.p.value=TRUE)
-
-table(quantitative$attrition_flag_binary, quantitative$Total_Amt_Chng_Q4_Q1)
-chisq.test(quantitative$attrition_flag_binary, quantitative$Total_Amt_Chng_Q4_Q1, correct=FALSE, simulate.p.value=TRUE)
-
-table(quantitative$attrition_flag_binary, quantitative$Total_Trans_Amt)
-chisq.test(quantitative$attrition_flag_binary, quantitative$Total_Trans_Amt, correct=FALSE, simulate.p.value=TRUE)
-
-table(quantitative$attrition_flag_binary, quantitative$Total_Trans_Ct)
-chisq.test(quantitative$attrition_flag_binary, quantitative$Total_Trans_Ct, correct=FALSE, simulate.p.value=TRUE)
-
-table(quantitative$attrition_flag_binary, quantitative$Total_Ct_Chng_Q4_Q1)
-chisq.test(quantitative$attrition_flag_binary, quantitative$Total_Ct_Chng_Q4_Q1, correct=FALSE, simulate.p.value=TRUE)
-
-table(quantitative$attrition_flag_binary, quantitative$Avg_Utilization_Ratio)
-chisq.test(quantitative$attrition_flag_binary, quantitative$Avg_Utilization_Ratio, correct=FALSE, simulate.p.value=TRUE)
-
-table(quantitative$attrition_flag_binary, quantitative$Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1)
-chisq.test(quantitative$attrition_flag_binary, quantitative$Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1, correct=FALSE, simulate.p.value=TRUE)
-
-
-#histograms
-hist(bank_data$Avg_Utilization_Ratio)
-hist(log1p(bank_data$Avg_Utilization_Ratio))
-
-hist(bank_data$Avg_Open_To_Buy)
-hist(log1p(bank_data$Avg_Open_To_Buy))
-hist(bank_data$Avg_Open_To_Buy)
